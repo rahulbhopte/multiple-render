794c7679f70b42369f2915429dbc6b3d3deb69ae
rahulbhopte <96414452+rahulbhopte@users.noreply.github.com>
2022-02-12 15:33:22 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>Name</h1>
+    <p>this is my project</p>
+  </div>,
+  rootElement
+);
